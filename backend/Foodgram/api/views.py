53095c1a7ec8bbd4f44209c9c18926fe715a5125
Артем Баландин <tom9981@yandex.ru>
2023-01-07 15:42:09 +0500

    project_spr14_04.01_rew4_shopping_cart_more_than_1page

--- backend/Foodgram/api/views.py
+++ backend/Foodgram/api/views.py
@@ -3,6 +3,7 @@ from django.http import FileResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action, permission_classes
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 
 from recipes.models import (FavoriteRecipe, Ingredient, IngredientRecipe,
--- backend/Foodgram/services/create_pdf.py
+++ backend/Foodgram/services/create_pdf.py
@@ -10,35 +10,51 @@ from reportlab.pdfgen import canvas
 
 def create_pdf(data: list, title: str) -> TextIO:
     """ Создает pdf-файл c помощью ReportLab."""
-
     buffer = io.BytesIO()
     p = canvas.Canvas(buffer, pagesize=A4)
-    pdfmetrics.registerFont(
-        TTFont('AlfiosBold', './static/AlfiosBold.ttf')
-    )
-    pdfmetrics.registerFont(TTFont(
-        'AlfiosRegular', './static/AlfiosRegular.ttf')
-    )
+    items_per_page = 20
+    Pdf_pages = max(0, (data.count()-1)) // items_per_page + 1
 
-    p.setFont('AlfiosBold', 24)
-    y = 800
-    p.setFillColor(olive)
-    p.drawString(60, y, f'{title}:')
-    y -= 40
-
-    p.setFont('AlfiosRegular', 16)
-    p.setFillColor(navy)
-    string_number = 1
-    for i in data:
-        p.drawString(
-            60, y,
-            f'{string_number}. {i[0].capitalize()} ({i[1]}) - {i[2]}'
+    counter = 0
+    while counter < Pdf_pages:
+        pdfmetrics.registerFont(
+            TTFont('AlfiosBold', './static/AlfiosBold.ttf')
         )
-        y -= 25
-        string_number += 1
+        pdfmetrics.registerFont(TTFont(
+            'AlfiosRegular', './static/AlfiosRegular.ttf')
+        )
 
-    p.showPage()
+        p.setFont('AlfiosBold', 24)
+        y = 800
+        p.setFillColor(olive)
+        p.drawString(
+            60,
+            y,
+            f'{title}'
+            f' ({items_per_page}/{data.count()-items_per_page*counter}'
+            f' поз., стр: {counter+1}/{Pdf_pages}):'
+        )
+        y -= 40
+
+        p.setFont('AlfiosRegular', 16)
+        p.setFillColor(navy)
+        start_position = counter * items_per_page
+        for i in range(start_position, data.count()):
+            p.drawString(
+                60,
+                y,
+                f'{list(enumerate(data))[i][0]+1}. '  # №
+                f'{list(enumerate(data))[i][1][0].capitalize()} '  # ingredient
+                f'({list(enumerate(data))[i][1][1]}) - '  # measurement_unit
+                f'{list(enumerate(data))[i][1][2]}'  # total
+            )
+            y -= 40
+
+        p.showPage()
+        counter += 1
+
     p.save()
+
     buffer.seek(0)
 
     return buffer
